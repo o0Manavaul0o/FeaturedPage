39151e5be950205297d5dd696edaaa6f49d27e95
o0Manavaul0o <87138886+o0Manavaul0o@users.noreply.github.com>
2021-07-27 11:58:03 -0500

    Initial Commit

---
+++ FeaturedPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FeaturedPage
+//
+//  Created by Student42 on 7/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
